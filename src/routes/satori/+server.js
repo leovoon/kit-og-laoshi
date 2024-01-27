35ddd5d78887d865d96d957106e999865f9cd1ef
Leo Voon <voonlihhaur@gmail.com>
2024-01-27 12:16:38 +0800

    og endpoint config edge

--- src/routes/satori/+server.js
+++ src/routes/satori/+server.js
@@ -2,6 +2,11 @@ import Image from '../../lib/Dots.svelte';
 import { parseQuery } from '$lib/parse';
 import { componentToPng } from '$lib/renderImage';
 
+/** @type {import('@sveltejs/adapter-vercel').Config} */
+export const config = {
+	runtime: 'edge'
+};
+
 /** @type {import('./$types').RequestHandler} */
 export const GET = async ({ url }) => {
 	const query = url.searchParams;
